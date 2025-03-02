cc7708b83fbc0137e8c93a45c781e2602f4cc708
Luis Novaes <lfmnovaes@gmail.com>
2025-03-01 22:23:16 -0300

    Nextjs init, daisyui, prettier and cleanup

---
+++ app/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
+
+const inter = Inter({
+  subsets: ['latin'],
+  display: 'swap',
+});
+
+export const metadata: Metadata = {
+  title: 'Shift Hub',
+  description: 'Work shift management application',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" data-theme="light">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
